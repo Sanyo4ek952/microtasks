378a033f28ad93d24ea2fa6b8f60e10651956317
Sanyo4ek952 <Sanyo4ek952@gmail.com>
2024-05-18 18:04:11 +0300

    Button Lesson

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,28 @@ import {Header} from "./site/Header";
 import {Body} from "./site/Body";
 import {Footer} from "./site/Footer";
 import {Map} from "./site/Map";
+import {Button} from "./components/Button";
 
 function App() {
+    const Button1Foo = (subscriber:string, age:number) => {
+        console.log(subscriber,age)
+    }
+    const Button2Foo = (subscriber:string, age?:number) => {
+        console.log(subscriber)
+    }
+    const Button3Foo = () => {
+        console.log("stupid button")
+    }
+
     return (
         <>
-            <Header title={"Header"}/>
+            {/*            <Header title={"Header"}/>
             <Body titleForBody={"Body"}/>
             <Footer titleForFooter={"Footer"}/>
-            <Map/>
+            <Map/>*/}
+            <Button name={"My button 1"} callBack={()=>Button1Foo("Ivan",21)}/>
+            <Button name={"My button 2"} callBack={()=>Button2Foo("Vasya")}/>
+            <Button name={"Stupid"} callBack={Button3Foo}/>
         </>
     )
 }
